cc4e2571d7e77e95f87622c12fe47c285c053291
Marcin Płużyński <marcinp2012@gmail.com>
2018-04-24 14:01:40 +0200

    TodoList comp added

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
 class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(event) {
+        if (this._inputElement !== '') {
+            const newItem = {
+                text: this._inputElement.value,
+                key: Date.now().toString(32)
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+
+            this._inputElement.value = '';
+        }
+
+        console.log(this.state.items);
+
+        event.preventDefault();
+    };
+
     render() {
         return (
             <div className='todoListMain'>
                 <div className='header'>
-                    <form>
-                        <input placeholder='enter task'/>
+                    <form onSubmit={this.addItem}>
+                        <input ref={ (a) => this._inputElement = a }
+                               placeholder='enter task'/>
                         <button type='submit'>add</button>
                     </form>
                 </div>
